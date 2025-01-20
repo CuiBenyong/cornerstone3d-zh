eb5fee8e6bfa0a284c68813d41c6927f02228a53
CuiBenyong <shl1018@hotmail.com>
2025-01-20 10:45:04 +0800

    chore: 配置域名

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,7 +10,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // 在这里设置你网站的生产 URL
-  url: 'https://cc3d-zh.netlify.app',
+  url: 'https://worldrecently.news',
   // 设置网站的 /<baseUrl>/ 路径
   // 对于 GitHub Pages 部署，通常是 '/<projectName>/'
   baseUrl: '/',
